b05938dff256261733bfc73fdd689ce0bba4115c
recep-demir <demir.rp@gmail.com>
2025-04-03 22:21:54 +0200

    add: incomeExpenses component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Balance from './components/Balance';
 import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
       <Header/>
       <div className="container">
         <Balance/>
+        <IncomeExpenses/>
       </div>
+
     </div>
   );
 }
