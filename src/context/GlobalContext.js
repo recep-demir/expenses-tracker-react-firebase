83272ea46da26a63a1da37234bfeef8281f1fae7
recep-demir <demir.rp@gmail.com>
2025-04-06 18:00:41 +0200

    refactor: reorganize component structure

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { toastSuccess } from '../helpers/ToastNotify';
 
 
 export const GlobalContext = createContext();
 
 
 const GlobalProvider = ({children}) => {
-const [transaction,setTransaction] = useState(()=>{
+const [transactions,setTransactions] = useState(()=>{
   const saved = localStorage.getItem('transactions')
   return saved ? JSON.parse(saved) : [];
 })
 
+useEffect(() => {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}, [transactions]);
+
+const addTransaction = (transaction) => {
+  transactions.some(item => item.id === transaction.id)
+    ? null
+    : (setTransactions([transaction, ...transactions]), toastSuccess("Transaction added"));
+};
+
+const deleteTransaction = (id) =>{
+  setTransactions(transactions.filter (item =>item.id !== id));
+  toastSuccess("Transaction added");
+}
 
   return (
-    <div>GlobalProvider</div>
+    
+    <GlobalContext.Provider value={{ transactions, addTransaction, deleteTransaction }}>
+      {children}
+    </GlobalContext.Provider>
   )
 }
 
