dcfc9388161f5cc15cf93328cbebe10db4ed3b67
recep-demir <demir.rp@gmail.com>
2025-04-06 17:18:51 +0200

    add: toastify

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export const GlobalContext = createContext();
+
+
+
+const GlobalProvider = ({children}) => {
+
+
+  
+  return (
+    <div>GlobalProvider</div>
+  )
+}
+
+export default GlobalProvider
--- src/context/GlobalState.js
+++
@@ -1,9 +0,0 @@
-
-
-const initialState = {
-    transactions: []
-};
-
-export const AppReducer = (state,action ) => {
-    
-}
