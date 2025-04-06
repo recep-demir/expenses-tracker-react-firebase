81497c436ca135b6bf252835ebb4cbd37b5986ff
recep-demir <demir.rp@gmail.com>
2025-04-06 19:35:24 +0200

    reduce was used

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
 
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext)
+  // const amounts = transactions.map(transaction => transaction.amount)
+
+  const total = transactions.reduce ((acc,item)=> (acc += item.amount), 0).toFixed(2)
+
   return (
     <div>
     <h4>Your Balance</h4>
-    <h1 className="balance">$0.00</h1>
+    <h1 className="balance">${total}</h1>
     </div>
   )
 }
