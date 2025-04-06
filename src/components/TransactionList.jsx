8a0c80f51f587da2b9f9df8636d31b20b8791eda
recep-demir <demir.rp@gmail.com>
2025-04-06 18:52:26 +0200

    up

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,17 +2,17 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalContext'
 
 const TransactionList = () => {
-  const context = useContext(GlobalContext)
+  const transactions = useContext(GlobalContext)
 
   
-
   return (
     <div>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+        {transactions.map(transaction => (<li className="minus">
+          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+        </li>) )}
+        
       </ul>
     </div>
   )
