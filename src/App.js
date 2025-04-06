49c5fef393982a021535f46c00b646e22f06164e
recep-demir <demir.rp@gmail.com>
2025-04-06 22:28:14 +0200

    feat: add tostcontainer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
@@ -16,6 +17,7 @@ function App() {
         <IncomeExpenses/>
         <TransactionList/>
         <AddTransaction/>
+        <ToastContainer/>
       </div>
       </GlobalProvider>
 
