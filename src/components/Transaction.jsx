a9997672506c619f5ccdc52ad53814d645cf0514
recep-demir <demir.rp@gmail.com>
2025-04-06 21:40:31 +0200

    up

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext';
 
 const Transaction = ({transaction}) => {
+  const {deleteTrasaction} = useContext(GlobalContext)
+
   const sign = transaction.amaount < 0 ? '-' : '+';
   return (
     <li className={transaction}>
-          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={()=>deleteTrasaction(transaction.id)} className="delete-btn">x</button>
         </li>
   )
 }
